0e6a15a33200d28afdcc2c861e7046325819f8d0
ashli_xyn <baucaashley6@gmail.com>
2024-10-03 11:56:35 +0800

    Update Bauca_04BasicOutput.cpp

--- Bauca_04BasicOutput.cpp
+++ Bauca_04BasicOutput.cpp
@@ -13,7 +13,7 @@ int main()
 	cout << "Course-Year Level: ";
 	cin >> course ;
 	cout << "School: ";
-	cin >> school ;
-	cout << "Wow Congratulations";
+	cin >> school ;  
+	cout << "Wow Congratulations" << nickname << "! " << cyl << is a nice course. And you are studying in" << school << ", which is one of the Centers of Excellence in Tertiary Education." <<endl;
 	return 0;
 }
