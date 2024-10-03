a095855df8012d27ae74285a84421d1f6b430f84
ashli_xyn <baucaashley6@gmail.com>
2024-10-03 10:43:51 +0800

    Add files via upload

---
+++ Bauca_02BasicOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Science" << endl;
+	cout << " BSIT-1B " <<endl;
+	cout << "Purok-1 Brgy. Santa Cruz Labo Camarines Norte" << endl;
+	return 0;
+
+}
